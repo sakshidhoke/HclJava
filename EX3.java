43e9801ff5605b4f941f29dd74f99a1bb0049328
sakshidhoke <91282406+sakshidhoke@users.noreply.github.com>
2021-09-28 12:16:30 +0530

    created a class and calling from main class

---
+++ EX3.java
@@ -0,0 +1,10 @@
+package Example;
+
+public class EX3 {
+
+	
+
+	int id=123344;
+	String name="Ketki";
+
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+package Example;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+EX3 emp=new EX3();             //emp is called as REFERENCE VARIABLE
+EX3 emp1=new EX3();             ///we can created multiple VARIABLE FOR THE SAME CALSS
+emp1.id=678;
+emp1.name="sakshi";
+System.out.println(emp.id);
+System.out.println(emp.name);
+System.out.println(emp1.name);
+System.out.println(emp1.id);
+	}
+
+}
+///in ex3 i have iniatize and in main class i am calling it this we can also do in oops
