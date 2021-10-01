15cc65bc0ed29adc971acb3c6f0a99fbc555e81d
sakshidhoke <91282406+sakshidhoke@users.noreply.github.com>
2021-10-01 09:12:10 +0530

    delete keyword in string

---
+++ Delete.java
@@ -0,0 +1,12 @@
+package StringBuffer;
+
+public class Delete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StringBuffer sb=new StringBuffer("SAKSHI");
+sb.delete(1, 3);
+System.out.println(sb);/////////1=A to 3 except 3
+	}
+
+}
