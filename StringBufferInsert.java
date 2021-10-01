f004bf86f9c5c1e839ad71a25826364ad761f0e4
sakshidhoke <91282406+sakshidhoke@users.noreply.github.com>
2021-10-01 09:10:48 +0530

    StringBuffer

---
+++ StringBufferInsert.java
@@ -0,0 +1,12 @@
+package StringBuffer;
+
+public class StringBufferInsert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StringBuffer sb=new StringBuffer("hello");
+sb.insert(5, "java");  
+System.out.println(sb);
+	}
+
+}
