bbffd28cb533b374b751b12025a13906a05ad664
sakshidhoke <91282406+sakshidhoke@users.noreply.github.com>
2021-10-08 23:01:24 +0530

    EBOX

---
+++ Assi9.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+public class Assi9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3;
+Scanner sc=new Scanner(System.in);
+int x=sc.nextInt();
+for(int i=0;i<x;i++) {
+	System.out.println(a+"");
+	a=a*3;
+}
+	}
+
+}
