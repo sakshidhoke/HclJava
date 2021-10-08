725737bfb01ec8096a80e279aa11e08b1859dbe7
sakshidhoke <91282406+sakshidhoke@users.noreply.github.com>
2021-10-08 22:59:01 +0530

    I Assess 1

---
+++ Assi1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.*;
+public class Assi1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char a,b,c;
+Scanner sc= new Scanner(System.in);
+System.out.println("enter 1 element");
+a=sc.next().charAt(0);
+System.out.println("enter 2 element");
+b=sc.next().charAt(0);
+System.out.println("enter the 3 element");
+c=sc.next().charAt(0);
+System.out.println(a+""+b+""+c);
+
+
+	}
+
+}
