f831bdecebe48c2d16777eb51089215776f00be5
sakshidhoke <91282406+sakshidhoke@users.noreply.github.com>
2021-10-08 23:00:04 +0530

    EBOX

---
+++ Assi4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+public class Assi4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the year");
+		int number=sc.nextInt();
+		System.out.println("Java was created on 1991.I am learning java on "+number);
+
+	}
+
+}
