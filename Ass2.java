8e2794cc05b2f3be133afccc7143d92b2df41c0f
sakshidhoke <91282406+sakshidhoke@users.noreply.github.com>
2021-10-08 22:59:24 +0530

    I Assess 2

---
+++ Ass2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.*;
+public class Ass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,y=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the bird name");
+x=sc.nextInt();
+//System.out.println("Enter the element 2");
+//y=sc.nextInt();
+y=((x/10)+(x%10));
+System.out.println(y);
+	}
+
+}
