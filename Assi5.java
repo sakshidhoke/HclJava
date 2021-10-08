f653bc1a6e5ea5e0311fcf32153d53605757beb9
sakshidhoke <91282406+sakshidhoke@users.noreply.github.com>
2021-10-08 23:00:24 +0530

    EBOX

---
+++ Assi5.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.Scanner;
+public class Assi5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the total no.over ");
+		double x=sc.nextDouble();
+		System.out.println("Target runs ");
+		int y=sc.nextInt();
+		System.out.println("Over bowled");
+		int z=sc.nextInt();
+		System.out.println("Run Scored");
+		int w=sc.nextInt();
+		int total=z/w;
+		System.out.println("Current run rate "+total);
+		
+
+	}
+
+}
